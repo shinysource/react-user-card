77314a77b1f9d64657734839dc5cc9a6c04a1096
shinysource <highly.flores@gmail.com>
2022-08-19 22:47:15 -0700

    Updated service api

--- src/service/api.tsx
+++ src/service/api.tsx
@@ -1,27 +1,19 @@
 import axios from "axios";
 
-const getUser = async () => {
-  try {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    return response;
-  } catch (error) {
-    console.log(error);
-  }
+import { User } from "../types";
+
+const getUser = () => {
+  const response = axios.get("https://jsonplaceholder.typicode.com/users");
+  return response;
 };
 
-const getAvatar = async (user_name: string) => {
-  try {
-    const response = await axios.get(
-      "https://avatars.dicebear.com/v2/avataaars/{{" +
-        user_name +
-        "}}.svg?options[mood][]=happy"
-    );
-    return response;
-  } catch (error) {
-    console.log(error);
-  }
+const getAvatar = (user_name: string) => {
+  const response = axios.get(
+    "https://avatars.dicebear.com/v2/avataaars/{{" +
+      user_name +
+      "}}.svg?options[mood][]=happy"
+  );
+  return response;
 };
 
 export { getUser, getAvatar };
